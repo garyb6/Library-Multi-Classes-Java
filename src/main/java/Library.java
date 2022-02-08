e141c1c9c0b7f04c987e4280180960d48b69d154
garyb6 <gary.bennett6@outlook.com>
2022-02-08 16:08:00 +0000

    Library getters passed

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Library {
+    private String name;
+    private ArrayList<Book> collection;
+
+    public Library(String name){
+        this.name = name;
+        this.collection = new ArrayList<>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int bookCount(){
+        return this.collection.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+
+    Library library;
+    Book book;
+
+    @Before
+    public void before(){
+        library = new Library("Central");
+        book = new Book("Biological Universe", "Wallace Arthur", "Science");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Central", library.getName());
+    }
+
+    @Test
+    public void collectionStartsEmpty(){
+        assertEquals(0, library.bookCount());
+    }
 }
